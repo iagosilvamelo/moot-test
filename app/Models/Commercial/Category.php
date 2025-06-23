b6f623e6c222668e562100b0f3b6d51fb952d610
Iago Fabricio Silva Melo <iagosilvamelo@gmail.com>
2025-06-23 11:59:16 -0300

    Criado estrutura de tabelas

---
+++ app/Models/Commercial/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Commercial;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ app/Models/Commercial/Product.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models\Commercial;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'category_id',
+        'manufacturer_id',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'created_at',
+        'updated_at'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function manufacturer(): BelongsTo
+    {
+        return $this->belongsTo(Manufacturer::class);
+    }
+}
