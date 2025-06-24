d2f4711da3cdc5187df13611b4c4bcc9a673e31a
Iago Fabricio Silva Melo <iagosilvamelo@gmail.com>
2025-06-24 17:02:10 -0300

    Fix: Adicionado classe de component para atender ao fluxo de CI

---
+++ app/View/Components/Product/Filters.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\View\Components\Product;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Collection;
+use Illuminate\View\Component;
+
+class Filters extends Component
+{
+    public function __construct(
+        public Collection $categories,
+        public Collection $manufacturers,
+        public string $searchNameBinding,
+        public string $selectedCategoriesBinding,
+        public string $selectedManufacturersBinding,
+        public string $clearFiltersAction
+    ) {}
+
+    public function render(): View|Closure|string
+    {
+        return view('components.product.filters');
+    }
+}
